8fca30627db106946d4db5e58a99e17e73f97b4d
gujitao001 <gujitao001@ke.com>
2020-08-07 16:56:36 +0800

    WIP:Dash Label

---
+++ lib/custom_widget/dash_lable.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class DashLabel extends Widget {
+  @override
+  Element createElement() {
+    // TODO: implement createElement
+    Text();
+    Opacity()
+    return 
+  }
+
+}
